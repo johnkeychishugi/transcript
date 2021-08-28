366f44bd66a1a149ecff230eb9fe222b00badac7
John Chishugi <jkchishugi@gmail.com>
2021-08-28 15:36:01 +0100

    ft-code-format

--- index.js
+++ index.js
@@ -7,31 +7,31 @@ import routers from './routers/';
 
 const app = express();
 
-app.engine('handlebars',exphbs({ defaultLayout:'main' }));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //Body Parser
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //set static folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //initialize Routers
 app.use(routers);
-app.get('/',(req,res)=> res.render('index'));
+app.get('/', (req, res) => res.render('index'));
 
 const PORT = process.env.PORT || 5000;
 
 // database connexion test
-const testDbConnexion = async() =>{
-    try {
-      await database.authenticate();
-      console.log('Database Successfully Connected');
-    } catch (error) {
-       console.error('Error',error);
-    }
-  };
+const testDbConnexion = async () => {
+  try {
+    await database.authenticate();
+    console.log('Database Successfully Connected');
+  } catch (error) {
+    console.error('Error', error);
+  }
+};
 
-  testDbConnexion();
+testDbConnexion();
 
-app.listen(PORT,console.log(`Server run on port ${PORT}`));
+app.listen(PORT, console.log(`Server run on port ${PORT}`));
