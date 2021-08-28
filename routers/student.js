bc8683796e013d357bbe0e12a90a64bb73188b0d
John Chishugi <jkchishugi@gmail.com>
2021-08-29 00:01:24 +0100

    ft-delete-student-route

--- routers/student.js
+++ routers/student.js
@@ -4,6 +4,7 @@ import studentController from '../controllers/studentController';
 const routers = express.Router();
 
 routers.get("/create",studentController.createView)
-        .post("/message",studentController.message);
+        .post("/store",studentController.store)
+        .post("/destroy/:id",studentController.destroy);
     
 export default routers
