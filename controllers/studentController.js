fbcc22bac6b5a28070c67acf89da441233038954
John Chishugi <jkchishugi@gmail.com>
2021-08-28 15:36:13 +0100

    ft-student-controller

---
+++ controllers/studentController.js
@@ -0,0 +1,40 @@
+const messageController = {
+    messageShow : (req,res) =>{
+        res.render('message',{ layout:'landing' });
+    },
+
+    message : (req,res) => {
+        let id 
+        if(messages.datas.length != 0){
+          id = messages.datas[messages.datas.length-1].id+1;
+        }else{
+          id = 1;
+        }
+        const created_at = new Date().toDateString(); 
+        console.log(created_at);
+        const data = new Message.DataMessage(req.body,id,created_at);
+
+        messages.save(data).then(respons =>{
+            messages.findAll().then(response =>{
+                res.render('home',{ 
+                    messages: response,
+                    layout:'landing'
+                });
+            });  
+        });  
+        
+    },
+
+    messages : (req,res) => {
+        messages.findAll().then(response =>{
+            res.render('home',{ 
+                messages: response,
+                layout:'landing'
+            });
+        });  
+    },
+
+    
+}
+
+export default messageController;
