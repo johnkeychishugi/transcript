a3244725a8d54ddefff24b48e73469ab54a88db3
John Chishugi <jkchishugi@gmail.com>
2021-08-29 16:10:08 +0100

    ft-route-section

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import studentRouter from './student';
+import sectionRouter from './section';
+
 
 const router = express.Router();
 
-router.use('/students',studentRouter);
-
+router.use('/students',studentRouter)
+      .use('/sections',sectionRouter);
 
 export default router;
