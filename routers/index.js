b3be736f6569296386d69fe631f32b5d2d9e2de8
John Chishugi <jkchishugi@gmail.com>
2021-08-29 16:29:16 +0100

    ft-router-department

--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import studentRouter from './student';
 import sectionRouter from './section';
-
+import departmentRouter from './department';
 
 const router = express.Router();
 
 router.use('/students',studentRouter)
-      .use('/sections',sectionRouter);
+      .use('/sections',sectionRouter)
+      .use('/departments',departmentRouter);
 
 export default router;
