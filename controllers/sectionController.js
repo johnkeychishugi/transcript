97e6332b06627ed3485c240d5ef933fa31953b42
John Chishugi <jkchishugi@gmail.com>
2021-08-29 16:09:20 +0100

    ft-section-controller

---
+++ controllers/sectionController.js
@@ -0,0 +1,17 @@
+import instructor from '../models/instructor';
+import section from '../models/section';
+
+const sectionController = {
+    getAll: (req,res) =>{
+        section.findAll({
+            order: [['id','DESC']],
+            include: [instructor]
+        }).then((data)=>{
+            res.render('section/index',{
+                sections: data
+            });
+        }).catch(error => console.log(error));
+    }
+};
+
+export default sectionController;
