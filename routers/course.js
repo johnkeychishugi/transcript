b4da60eadd0541596d660a6512899ac1a08ea676
John Chishugi <jkchishugi@gmail.com>
2021-08-29 17:04:43 +0100

    ft-code-format

--- routers/course.js
+++ routers/course.js
@@ -3,6 +3,6 @@ import courseController from '../controllers/courseController';
 
 const routers = express.Router();
 
-routers.get("/all",courseController.getAll);
-    
+routers.get("/all", courseController.getAll);
+
 export default routers
